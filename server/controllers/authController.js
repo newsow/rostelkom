69b336435241283d1acd9282775af27c44e516e4
newsow <yegor.shkil@mail.ru>
2023-11-23 20:07:56 +0300

    finished authorization on backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/userModel')
+const addressModel = require('../models/addressModel')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const secretKey = '1111'
@@ -10,13 +11,14 @@ const generateToken = (id,email)=>{
 class authController{
     async registration(req,res){
         try {
-            const {first_name,last_name,email,password} = req.body
+            const {first_name,last_name,email,password,address} = req.body
             const candidate = await userModel.findOne({email})
             if(candidate){
                 return res.status(400).json({message:'Пользователь с такой почты уже существует'})
             }
+            const user_address = await addressModel.findOne({address})
             const hashPassword = bcrypt.hash(password,3)
-            const user = await userModel.create({first_name,last_name,email,password:hashPassword})
+            const user = await userModel.create({first_name,last_name,email,password:hashPassword,address:user_address})
             const token = generateToken(user._id,user.email)
             return res.status(200).json({token})
         } catch (error) {
@@ -39,6 +41,16 @@ class authController{
             return res.status(400).json({message:error})
         }
     }
+    async getAdresses(req,res){
+        try {
+            const addresses = await addressModel.find({})
+            return res.status(200).json({addresses})
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({error})
+        }
+    }
+    
 
 }
 
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
-
+router.post('/registration',authController.registration)
+router.post('/login',authController.login)
+router.get('/adresses',authController.getAdresses)
 
 module.exports = router
