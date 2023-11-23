c0f033ef66a3b9ba10b5e4847134daca4c759006
newsow <yegor.shkil@mail.ru>
2023-11-23 21:59:45 +0300

    auth_front

--- client/src/pages/AppRouter.jsx
+++ client/src/pages/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Login from './Login'
+import Registration from './Registration'
 const AppRouter = () => {
   return (
     <div>
         <Routes>
             <Route path='/login' element={<Login/>}/>
+            <Route path='/registration' element={<Registration/>}/>
         </Routes>
     </div>
   )
