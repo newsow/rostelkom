90efcf88c0a6ac9016f1d6fdc22cd3c2b62b5451
bebres <ibreus750@gmail.com>
2023-11-24 16:26:49 +0300

    Nothing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import ListUsers from "./components/ListUsers";
 import MainChat from "./components/MainChat";
 import AppRouter from "./pages/AppRouter";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
       <AppRouter/>
-      <ListUsers/>
     </div>
   );
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import './css/Profile.css'
 
 const Profile = () => {
   return (
-    <div className='userProfile'></div>
+    <div>Profile</div>
   )
 }
 
